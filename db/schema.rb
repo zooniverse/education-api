ff5bd0be0f0613c55563089245c8ba1223b8f04b
Marten Veldthuis <marten@veldthuis.com>
2016-03-03 16:51:17 +0000

    Add classroom metadata

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125133030) do
+ActiveRecord::Schema.define(version: 20160303164604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20160125133030) do
     t.string   "join_token"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "school"
+    t.string   "subject"
+    t.text     "description"
   end
 
   create_table "groups", force: :cascade do |t|
