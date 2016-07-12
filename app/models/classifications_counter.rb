8110ec05b996750b7ccbe96a79d222b309c7423b
Marten Veldthuis <marten@veldthuis.com>
2016-07-12 13:59:23 +0100

    Count classifications made per assignment

--- app/models/classifications_counter.rb
+++ app/models/classifications_counter.rb
@@ -1,15 +1,22 @@
 class ClassificationsCounter
-  attr_reader :counts_by_user_group, :counts_by_user
+  attr_reader :counts_by_user_group, :counts_by_user, :counts_by_assignment
 
   def initialize
     @counts_by_user_group = {}
     @counts_by_user = {}
+    @counts_by_assignment = {}
   end
 
   def process(line)
     user_id = line["user_id"]
+    workflow_id = line["workflow_id"]
+
     counts_by_user[user_id] ||= 0
     counts_by_user[user_id] += 1
+
+    counts_by_assignment[workflow_id] ||= {}
+    counts_by_assignment[workflow_id][user_id] ||= 0
+    counts_by_assignment[workflow_id][user_id] += 1
 
     if line["metadata"].include?("user_group_ids")
       metadata = JSON.load(line["metadata"] || "{}")
@@ -25,6 +32,17 @@ class ClassificationsCounter
     StudentUser.joins(:user).where(users: {zooniverse_id: counts_by_user.keys}).find_each do |student_user|
       student_user.classifications_count = counts_by_user[student_user.zooniverse_id]
       student_user.save!
+    end
+
+    Assignment.where(workflow_id: counts_by_assignment.keys).find_each do |assignment|
+      workflow_id = assignment.workflow_id
+
+      assignment.student_assignments.each do |student_assignment|
+        user_id = student_assignment.student_user.user.zooniverse_id
+
+        student_assignment.classifications_count = counts_by_assignment[workflow_id][user_id]
+        student_assignment.save!
+      end
     end
 
     Classroom.where(zooniverse_group_id: counts_by_user_group.keys).find_each do |classroom|
