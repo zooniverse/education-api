6714f5fd421ea5ee12f0cf81b6e6a5622035cde9
Marten Veldthuis <marten@veldthuis.com>
2016-07-15 15:41:53 +0100

    Serialize classroom_id

--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,5 +1,5 @@
 class AssignmentSerializer < ActiveModel::Serializer
-  attributes :id, :name, :metadata, :workflow_id, :subject_set_id
+  attributes :id, :name, :metadata, :classroom_id, :workflow_id, :subject_set_id
   has_many :student_assignments
   has_many :student_users
 end
