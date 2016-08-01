1d0cc5c2c9f057323bb9bbf18c81587dbb1c7717
Marten Veldthuis <marten@veldthuis.com>
2016-08-01 13:55:47 +0100

    upload to cartodb in smaller batches

--- app/operations/classifications/process_export.rb
+++ app/operations/classifications/process_export.rb
@@ -10,9 +10,14 @@ module Classifications
       classifications_counter = ClassificationsCounter.new
       carto_transformer = CartoTransformer.new
 
-      CSV.foreach(export_path, headers: true) do |line|
+      CSV.foreach(export_path, headers: true).with_index do |line, index|
         classifications_counter.process(line) if line["user_id"]
         carto_transformer.process(line)
+
+        if index % 10000 == 0
+          # Do carto uploads in batches.
+          carto_transformer.finalize
+        end
       end
 
       classifications_counter.finalize
