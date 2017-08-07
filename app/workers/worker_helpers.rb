c4edc60844cf738d2be6f45da6295c317f04ec58
Marten Veldthuis <marten@veldthuis.com>
2017-08-07 15:18:59 +0100

    Set env in client

--- app/workers/worker_helpers.rb
+++ app/workers/worker_helpers.rb
@@ -1,6 +1,7 @@
 module WorkerHelpers
   def client
     @client ||= Panoptes::Client.new \
+      env: Rails.env.to_sym,
       auth: {client_id: Rails.application.secrets["zooniverse_oauth_key"],
              client_secret: Rails.application.secrets["zooniverse_oauth_secret"]}
   end
