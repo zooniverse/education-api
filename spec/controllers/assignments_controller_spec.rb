04c165adac2f655c4999fb1b3da583a16cec192e
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2017-10-04 15:25:04 -0500

    Re-added broken assignment#create controller op spec

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -25,9 +25,24 @@ RSpec.describe AssignmentsController do
       program = create(:program)
       classroom  = create(:classroom, program: program, teachers: [current_user])
       assignment = build(:assignment, classroom: classroom)
+      outcome = double(result: assignment, valid?: true)
 
       attributes = {workflow_id: 8888, name: "Foo"}
       relationships = {classroom: {data: {id: classroom.id, type: 'classrooms'}}}
+
+
+      rebuilt = {}.tap do |param|
+        param[:attributes] = attributes
+        param[:relationships] = relationships
+        param[:current_user] = current_user
+        param[:client] = panoptes_application_client
+      end
+
+      action_params = ActionController::Parameters.new(rebuilt)
+
+      expect(Assignments::Create).to receive(:run)
+        .with(action_params)
+        .and_return(outcome)
 
       post :create, params: {data: {attributes: attributes, relationships: relationships}}, format: :json
       expect(parsed_response[:data][:attributes][:name]).to eq(assignment.name)
