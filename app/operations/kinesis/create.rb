e34694ac9ee6924c31f6d982eba118e0931caf77
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2017-12-04 17:22:02 +0000

    Add project model and association with programs

--- app/operations/kinesis/create.rb
+++ app/operations/kinesis/create.rb
@@ -17,14 +17,16 @@ module Kinesis
     def process(stream_event)
       return unless stream_event.fetch("source") == "panoptes"
       return unless stream_event.fetch("type") == "classification"
-      return unless stream_event.fetch("data").fetch("links").fetch("project") == "593"
+
+      project = Project.find_by_id(stream_event.fetch("data").fetch("links").fetch("project"))
+      return unless project
 
       if stream_event.fetch("data").fetch("links").fetch("user").present? &&
          stream_event.fetch("data").fetch("metadata").fetch("user_group_ids").present?
         Kinesis::CountClassification.run! stream_event
       end
 
-      carto_transformer.process(stream_event)
+      carto_transformer.process(stream_event) if project.program.custom?
     end
 
     def carto_transformer
