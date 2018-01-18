7792bc2fccd96fc573988fb2c25aeaaec99c1644
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2018-01-18 13:28:44 -0600

    Non-UG assignment counters shouldn't increment
    A classification comes in over the kinesis stream. If the `selected_user_group_id` param is blank, increment all the assignment counters as expected (Wildcam). However, if that param exists, only update the StudentAssignment counter if that assignment's classroom's zooniverse group ID matches. This was already fixed for classrooms.

--- spec/operations/kinesis/count_classification_spec.rb
+++ spec/operations/kinesis/count_classification_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Kinesis::CountClassification do
   let(:payload_sans_user_group) { payload.tap { |p| p["data"]["metadata"].delete("selected_user_group_id") } }
   let(:operation_sans_user_group) { described_class.with(payload_sans_user_group) }
 
-  let(:student)   { create :user, zooniverse_id: "1234" }
-  let(:classroom) { create :classroom, zooniverse_group_id: "1234", students: [student] }
+  let!(:student)   { create :user, zooniverse_id: "1234" }
+  let!(:classroom) { create :classroom, zooniverse_group_id: "1234", students: [student] }
+  let!(:non_ug_classroom) { create :classroom, zooniverse_group_id: "9999", students: [student] }
 
   context "with user group" do
     it 'increments counter for the classroom' do
@@ -19,9 +20,18 @@ RSpec.describe Kinesis::CountClassification do
       expect { operation.run! }.to change { classroom.student_users.first.reload.classifications_count }.from(0).to(1)
     end
 
+    it 'only increments the appropriate counter' do
+      expect { operation.run! }.not_to change { non_ug_classroom.reload.classifications_count }
+    end
+
     it 'increments counter for a student in an assignment' do
       create :assignment, classroom: classroom, student_users: classroom.student_users, workflow_id: "4378"
       expect { operation.run! }.to change { classroom.student_users.first.student_assignments.first.reload.classifications_count }.from(0).to(1)
+    end
+
+    it 'only increments the assignment counter for the appropriate classroom' do
+      create :assignment, classroom: non_ug_classroom, student_users: non_ug_classroom.student_users, workflow_id: "4378"
+      expect { operation.run! }.not_to change { non_ug_classroom.student_users.first.student_assignments.first.reload.classifications_count }
     end
   end
 
