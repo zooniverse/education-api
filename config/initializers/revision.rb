874b35e8388f6f0091b4e2732eb4f57cc7ad9eff
Campbell Allen <campbell.allen@gmail.com>
2021-05-27 18:23:08 +0100

    add revision via ENV var

--- config/initializers/revision.rb
+++ config/initializers/revision.rb
@@ -1,5 +1,2 @@
-revision_file = Rails.root.join 'public/commit_id.txt'
+Rails.application.revision = ENV.fetch('REVISION', '')
 
-if File.exist?(revision_file)
-  Rails.application.revision = File.read(revision_file).chomp
-end
